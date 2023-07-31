a12a881c5b595b548e5d90d2ad5780785498dcc7
rajas-goldenleaf <rajas@goldenleafdigital.com>
2023-07-31 19:34:28 +0000

    inital setup code settings

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
+import { useDeviceOrientation } from '@react-native-community/hooks';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from 'react';
+import { Dimensions, SafeAreaView, StyleSheet, Text, TouchableNativeFeedback, View } from 'react-native';
+
+const screenHeight = Dimensions.get("screen").height;
+const screenWidth = Dimensions.get("screen").width;
 
 export default function App() {
+  const orientation = useDeviceOrientation()
+  useEffect(() => {
+    console.log(orientation)
+  }, [orientation])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text>hello there</Text>
+        <View  style={styles.button} >
+          <TouchableNativeFeedback onPress={() => {
+            console.log("button pressed");
+          }}
+          >
+            <View>
+              <Text style={styles.text}>button pressed</Text>
+            </View>
+          </TouchableNativeFeedback>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    color: "#e6edf3",
   },
+  button: {
+    backgroundColor: "#1f6feb",
+    width: "fit-content",
+    padding: "20 0"
+  },
+  text: {
+    color: "#e6edf3",
+    fontWeight: "bold",
+
+  }
 });
