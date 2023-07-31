df2ffac0fc3b8192445a105b2ec441487f88a967
rajas-goldenleaf <rajas@goldenleafdigital.com>
2023-07-31 20:18:11 +0000

    adding assets

--- App.js
+++ App.js
@@ -1,31 +1,21 @@
 import { useDeviceOrientation } from '@react-native-community/hooks';
-import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
-import { Dimensions, SafeAreaView, StyleSheet, Text, TouchableNativeFeedback, View } from 'react-native';
+import { Dimensions,StatusBar, SafeAreaView, StyleSheet, Text, TouchableNativeFeedback, View } from 'react-native';
 
 const screenHeight = Dimensions.get("screen").height;
 const screenWidth = Dimensions.get("screen").width;
 
 export default function App() {
   const orientation = useDeviceOrientation()
+
   useEffect(() => {
     console.log(orientation)
   }, [orientation])
 
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        <Text>hello there</Text>
-        <View  style={styles.button} >
-          <TouchableNativeFeedback onPress={() => {
-            console.log("button pressed");
-          }}
-          >
-            <View>
-              <Text style={styles.text}>button pressed</Text>
-            </View>
-          </TouchableNativeFeedback>
-        </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+      <Text>Hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis laudantium assumenda pariatur repellat! Odit ullam quaerat vero fugit nesciunt, sapiente alias voluptates sequi omnis nulla suscipit, quidem est perspiciatis laudantium.</Text>
       </View>
     </SafeAreaView>
   );
@@ -33,16 +23,10 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    color: "#e6edf3",
-  },
-  button: {
-    backgroundColor: "#1f6feb",
-    width: "fit-content",
-    padding: "20 0"
-  },
-  text: {
-    color: "#e6edf3",
-    fontWeight: "bold",
-
+    flex: 1,
+    backgroundColor: "white",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    marginRight: Platform.OS === "android" ? 5  : 0,
+    marginLeft: Platform.OS === "android" ? 10  : 0,
   }
 });
