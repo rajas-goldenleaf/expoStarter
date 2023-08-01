35d673db117f6981313eda3adab16d73be3784e1
rajas-goldenleaf <rajas@goldenleafdigital.com>
2023-08-01 10:00:22 +0000

    using css as normal className with alias styleName
    important plugins
    babel-plugin-react-css-modules
    
    react-native-css-transformer

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { useDeviceOrientation } from '@react-native-community/hooks';
 import { useEffect } from 'react';
 import { Dimensions, StatusBar, SafeAreaView, StyleSheet, Text, TouchableNativeFeedback, View } from 'react-native';
-import "./Button.css"
+import "./Button.module.css"
 
 const screenHeight = Dimensions.get("screen").height;
 const screenWidth = Dimensions.get("screen").width;
@@ -14,24 +14,8 @@ export default function App() {
   }, [orientation])
 
   return (
-    // <SafeAreaView style={styles.container}>
-    // <SafeAreaView styleName="container">
-    //   <View>
-    //   <Text>Hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis laudantium assumenda pariatur repellat! Odit ullam quaerat vero fugit nesciunt, sapiente alias voluptates sequi omnis nulla suscipit, quidem est perspiciatis laudantium.</Text>
-    //   </View>
-    // </SafeAreaView>
-    <View styleName="wrapper">
-      <Text>Foo</Text>
-    </View>
+    <SafeAreaView>
+      <Text styleName="my-dashed-class">hello lorem some random name is present in here there are other things that are present here</Text>
+    </SafeAreaView>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "white",
-//     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-//     marginRight: Platform.OS === "android" ? 5  : 0,
-//     marginLeft: Platform.OS === "android" ? 10  : 0,
-//   }
-// });
