27ddd1ad4515bdd327a6f5a74c85e03b8edbdb77
rajas-goldenleaf <rajas@goldenleafdigital.com>
2023-08-19 20:36:16 +0000

    added lines

--- test.js
+++ test.js
@@ -5,3 +5,7 @@ console.log('this is new file added in the laster 3st commit');
 
 
 console.log("this is new changes made in this master so footer is now behind one commit this should give conflict as same line is changing let's see sure and short this creats conflict  !!")
+
+console.log("lets see now will it create merge conflict or straight fast-forward merge")
+
+console.log("some other lines which will be fast forwarded")
