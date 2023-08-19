84e206f955a6b3cdb338355ed5579a5dc4e77748
rajas-goldenleaf <rajas@goldenleafdigital.com>
2023-08-19 20:07:44 +0000

    modified same line

--- test.js
+++ test.js
@@ -1,1 +1,5 @@
 console.log("this is the code on footer branch")
+
+console.log("this is new changes made in this master so footer is now behind one commit this should give conflict as same line is changing let's see !!")
+
+console.log("some other lines which will be fast forwarded")
