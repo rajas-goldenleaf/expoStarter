903082177561ddf46a53c8d563f818b8ee11adac
rajas-goldenleaf <rajas@goldenleafdigital.com>
2023-08-19 20:29:31 +0000

    ahead of single commit

--- test.js
+++ test.js
@@ -1,7 +1,3 @@
 console.log("this is the code on footer branch")
 
 console.log("this is new changes made in this master so footer is now behind one commit this should give conflict as same line is changing let's see sure and short this creats conflict  !!")
-
-console.log("lets see now will it create merge conflict or straight fast-forward merge")
-
-console.log("some other lines which will be fast forwarded")
