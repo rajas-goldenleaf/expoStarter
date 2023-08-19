75d05572a6da19330a269540e82346771b88f9e3
rajas-goldenleaf <rajas@goldenleafdigital.com>
2023-08-19 20:37:49 +0000

    added 2nd ahead

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 console.log("this is the code on footer branch")
 console.log('this is new file added in the laster 1st commit');
+console.log('this is new file added in the laster 2st commit');
+
 
 console.log("this is new changes made in this master so footer is now behind one commit this should give conflict as same line is changing let's see sure and short this creats conflict  !!")
