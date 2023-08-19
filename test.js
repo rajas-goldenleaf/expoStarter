176d0fa0ba24422e09b1423d005745eb584d6ed5
rajas-goldenleaf <rajas@goldenleafdigital.com>
2023-08-19 06:00:42 +0000

    Revert "Revert "add bad commit""
    This reverts commit ac4e7ccafce6eb5a9e5e1186d9f65f9411680c7f.

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 #!/home/codespace/nvm/current/bin/node
 console.log('Hello there this is working ');
 function addition(a, b) {
-    return a + b;
+    throw new Error("it will give error")
+    // return a + b;
 }
 console.log(addition(10, 2));
 
